e4145ee22b31b529c18afe2b342a5222fde13812
Luis Martinez <luis_mmartinez@live.com>
2021-04-14 17:14:15 -0400

    commit tree 10bcdff44b3b2b7d31222bb5868a9a19434d97b5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+   get '/', to: 'products#index'
+  post '/products', to: 'products#add'
+
+  #resources :products, only: [:index, :create]
+  # root 'products#index'
 end
